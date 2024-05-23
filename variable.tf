26f1cce7e50a9a45863337202e41039e27819203
venkatkukatla <104503378+venkatkukatla@users.noreply.github.com>
2024-05-23 06:58:06 +0530

    vari

---
+++ variable.tf
@@ -0,0 +1,51 @@
+variable azurerm_resource_group_name {
+  type        = string
+  description = "description"
+}
+
+variable location {
+  type        = string
+  description = "description"
+}
+
+variable azurerm_virtual_network_name {
+  type        = string
+  description = "description"
+}
+
+variable address_space {
+  type        = list
+  description = "description"
+
+}
+variable azurerm_subnet1_name {
+  type        = string
+  description = "description"
+}
+
+variable address_prefixes1 {
+  type        = list
+  description = "description"
+}
+
+variable azurerm_subnet2_name {
+  type        = string
+  description = "description"
+}
+
+variable address_prefixes2 {
+  type        = list
+  description = "description"
+}
+
+variable azurerm_subnet3_name {
+  type        = string
+  description = "description"
+}
+
+variable address_prefixes3 {
+  type        = list
+  description = "description"
+}
+
+
